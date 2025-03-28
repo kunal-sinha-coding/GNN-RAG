3be3a8c471bb4bfa3b886d30ec8bee2d45071467
Kunal Sinha <ksinha2@jagupard28.stanford.edu>
2025-03-28 12:54:39 -0700

    Wipe files properly

--- gnn/generate_dataset.py
+++ gnn/generate_dataset.py
@@ -20,7 +20,7 @@ model = AutoModelForCausalLM.from_pretrained(
     torch_dtype=torch.float16
 ).to(device)
 
-DATA_NAME = "synth-fin-3"
+DATA_NAME = "synth-fin-0"
 FOLDER_NAME = os.path.join("data", DATA_NAME)
 LLAMA_PROMPT = (
     '''
@@ -194,12 +194,12 @@ def synthesize_step(qa_pairs, num_questions, id_num,
             subgraph, entity2id, relation2id, vocab
         )
 
-def save_qa_pairs(qa_pairs, num_questions, id_num, file_name):
+def save_qa_pairs(qa_pairs, num_questions, id_num, file_name, overwrite=False):
     qa_pairs_batch = qa_pairs[id_num : id_num + num_questions]
     if not os.path.isdir(FOLDER_NAME):
         os.mkdir(FOLDER_NAME)
     file_name = os.path.join(FOLDER_NAME, file_name)
-    permissions = "a" if os.path.isfile(file_name) and id_num > 0 else "w"
+    permissions = "a" if os.path.isfile(file_name) and not overwrite else "w"
     with open(file_name, permissions) as f:
         for pair in qa_pairs_batch:
             if "paths" in pair:
@@ -259,26 +259,30 @@ def synthesize(num_steps=10000, num_questions=10, num_generations=25, qa_file="a
                 relation2id=relation2id,
                 vocab=vocab
             )
-            save_qa_pairs(qa_pairs, num_questions, id_num, curr_qa_file)
+            save_qa_pairs(qa_pairs, num_questions, id_num, curr_qa_file, overwrite=(j==0))
             save_subgraph(qa_pairs, subgraph, entity2id, curr_qa_file)
             save_id_dicts(qa_pairs, entity2id, relation2id, vocab)
             idx += 1
         split_data(curr_qa_file, f"train{i}.json", [f"dev{i}.json"])
 
-def combine_data(dst_names=["train", "dev"], 
+def combine_data(dst_names="all",#["train", "dev"], 
                 src_names=["synth-fin", "synth-fin-1", "synth-fin-2", "synth-fin-3"]):
     for dst_name in dst_names:
-        with open(f"{dst_name}.json", "w") as f:
-            f.write("")
-        with open(f"{dst_name}.json", "a") as dst:
+        full_dst_name = os.path.join("data", src_names[0], f"{dst_name}.json")
+        with open(full_dst_name, "w") as dst:
+            dst.write("")
+        with open(full_dst_name, "a") as dst:
             for src_name in src_names:
+                import pdb; pdb.set_trace()
                 for file_name in os.listdir(os.path.join("data", src_name)):
                     if dst_name in file_name:
-                        with open(file_name, "r") as src:
+                        with open(os.path.join("data", src_name, file_name), "r") as src:
                             lines = src.readlines()
+                            print(len(lines))
+                            import pdb; pdb.set_trace()
                             dst.writelines(lines)
 
-# synthesize()
-combine_data()
+synthesize()
+#combine_data()
 
 
